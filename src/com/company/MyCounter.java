b5a460966138b3ca468a0e72d91c4cbab8502560
danCraw <66249493+danCraw@users.noreply.github.com>
2020-06-01 20:05:46 +0300

    Update MyCounter.java

--- src/com/company/MyCounter.java
+++ src/com/company/MyCounter.java
@@ -7,7 +7,7 @@ public class MyCounter {
 
     public  static Map countMeeting(String text){
         MyMap<String, Integer> myMap = new MyMapImplements<>();
-        for (String word : text.replaceAll("[^A-Za-zА-Яа-яЁё]", "").split(" ")) {
+        for (String word : text.replaceAll("[^A-Za-zА-Яа-яЁё ]", "").split(" ")) {
             if (word.length() > 1) {
                 if (word.substring(0, 1).toUpperCase().equals(word.substring(0, 1)) && !(word.substring(0, 2).toUpperCase().equals(word.substring(0, 2)))) {
                     myMap.countMeeting(word);
